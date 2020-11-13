165686aa6c81a529492686efefae62c870a0793d
KeryLong <kery.long@gmail.com>
2020-11-13 14:43:47 -0700

    volunteer form updates

---
+++ script.js
@@ -0,0 +1,16 @@
+let GroupMembers = document.getElementById("moreMembers");
+
+function toggleCheckbox() {
+    var moreMembers = document.getElementById("yesToMore").checked;
+    if (moreMembers) {
+      document.getElementById("moreMembers").style.display = "inline";
+    } else {
+      document.getElementById("moreMembers").style.display = "none";
+    }
+  }
+
+function addTeamMembers(name){
+  document.createElement(<input></input>)
+}
+
+  
