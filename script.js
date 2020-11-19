5b83917f078ce50594e6fe4d24646dfef743e23a
KeryLong <kery.long@gmail.com>
2020-11-19 15:05:46 -0700

    input add with Greg

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
 
-function addTeamMembers(){
+function addTeamMembers() {
   let appendDiv = document.getElementById("addTeamMemberDiv");
-  let textNode=document.createTextNode('ihkjhkjhkjhh');
-  appendDiv.appendChild('ihkjhkjhkjhh')
+  let firstName = document.createElement("input");
+
+  firstName.setAttribute('type', 'text');
+  firstName.setAttribute
+  appendDiv.appendChild(firstName);
+}
 //   let firstName = document.createElement("input");
   
 
 //   firstName.setAttribute('type', 'text');
+
 //   appendDiv.appendChild(firstName);
 //   document.getElementsByTagName("input").placeholder = "Team member first name";
-}
